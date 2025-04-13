0f9b24dee538adf3d463cc32c128ccbc94a97db4
PatGlock <devin.gloeckner@gmail.com>
2025-04-13 17:02:34 -0500

    Marked up checkCheck.java

--- checkCheck.java
+++ checkCheck.java
@@ -1,5 +1,5 @@
 import java.util.List;
-
+// I'll make the current player a parameter
 public static Boolean checkCheck(){
     List<Piece> board = Game.initBoard();
     int kingIndex = -1;
@@ -12,5 +12,8 @@ public static Boolean checkCheck(){
             kingIndex = i;
             playerColor = p.getColor();
             break;
+    //iterate through opposite sides pieces and see if taking king is a valid move
+    //if true, return false
+    //if finishes the loop and finds nothing, return true
         }
     }
