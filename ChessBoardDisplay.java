b9ac7911d3bd33ea49a427506457785b8dad06d8
Josiah Young <josiahyoung437@gmail.com>
2025-05-07 16:07:05 -0500

    Update ChessBoardDisplay.java
    added a way for the pieces to show will need to fit with rest of code i have a code that works for an entire game its not horribly long

--- ChessBoardDisplay.java
+++ ChessBoardDisplay.java
@@ -70,7 +70,48 @@ private void updateBoard(){
     public static void ChessBoardDisplay(String[] args) {
             ChessBoard chessBoard = new ChessBoard(); chessBoard.displayBoard();
         }
-    }
+    }public final void initPieceImages() {
+		// Black piece images
+		pieceImage_b[0] = new ImageIcon(new ImageIcon("./img/king_b.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_b[1] = new ImageIcon(new ImageIcon("./img/queen_b.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_b[2] = new ImageIcon(new ImageIcon("./img/bishop_b.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_b[3] = new ImageIcon(new ImageIcon("./img/knight_b.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_b[4] = new ImageIcon(new ImageIcon("./img/rook_b.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_b[5] = new ImageIcon(new ImageIcon("./img/pawn_b.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_b[6] = new ImageIcon(new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
+		
+		// White piece images
+		pieceImage_w[0] = new ImageIcon(new ImageIcon("./img/king_w.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_w[1] = new ImageIcon(new ImageIcon("./img/queen_w.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_w[2] = new ImageIcon(new ImageIcon("./img/bishop_w.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_w[3] = new ImageIcon(new ImageIcon("./img/knight_w.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_w[4] = new ImageIcon(new ImageIcon("./img/rook_w.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_w[5] = new ImageIcon(new ImageIcon("./img/pawn_w.png").getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+		pieceImage_w[6] = new ImageIcon(new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
+	}
+	
+	// Retrieves the image icon for a given chess piece.
+	public ImageIcon getImageIcon(Piece piece) {
+		if(piece.color.equals(PlayerColor.black)) {
+			if(piece.type.equals(PieceType.king)) return pieceImage_b[0];
+			else if(piece.type.equals(PieceType.queen)) return pieceImage_b[1];
+			else if(piece.type.equals(PieceType.bishop)) return pieceImage_b[2];
+			else if(piece.type.equals(PieceType.knight)) return pieceImage_b[3];
+			else if(piece.type.equals(PieceType.rook)) return pieceImage_b[4];
+			else if(piece.type.equals(PieceType.pawn)) return pieceImage_b[5];
+			else return pieceImage_b[6];
+		}
+		else if(piece.color.equals(PlayerColor.white)) {
+			if(piece.type.equals(PieceType.king)) return pieceImage_w[0];
+			else if(piece.type.equals(PieceType.queen)) return pieceImage_w[1];
+			else if(piece.type.equals(PieceType.bishop)) return pieceImage_w[2];
+			else if(piece.type.equals(PieceType.knight)) return pieceImage_w[3];
+			else if(piece.type.equals(PieceType.rook)) return pieceImage_w[4];
+			else if(piece.type.equals(PieceType.pawn)) return pieceImage_w[5];
+			else return pieceImage_w[6];
+		}
+		else return pieceImage_w[6];
+	}
 
 }
 
